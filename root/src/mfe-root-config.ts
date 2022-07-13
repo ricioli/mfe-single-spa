c25417a50d69499c03eedff80fc209643caf0700
Alex <alexricioli@outlook.com>
2022-07-13 17:55:41 +0000

    dynamic register applications

--- root/src/mfe-root-config.ts
+++ root/src/mfe-root-config.ts
@@ -1,20 +1,37 @@
-import { registerApplication, start } from "single-spa";
+import { registerApplication, start } from 'single-spa';
 
-registerApplication({
-  name: "@single-spa/welcome",
-  app: () =>
-    System.import(
-      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-    ),
-  activeWhen: ["/"],
-});
+interface IApp {
+  name: string;
+  url: string;
+  activeWhen: string[];
+  exact?: boolean;
+}
 
-// registerApplication({
-//   name: "@mfe/navbar",
-//   app: () => System.import("@mfe/navbar"),
-//   activeWhen: ["/"]
-// });
+const register = (app: IApp): void => {
+  registerApplication({
+    name: app.name,
+    app: () => System.import(app.url),
+    activeWhen: app.exact ? (location) => location.pathname === app.activeWhen[0] : app.activeWhen,
+  });
+};
 
-start({
-  urlRerouteOnly: true,
-});
+(async () => {
+  try {
+    const response = await fetch('');
+    const applications = await response.text().then(() => [
+      {
+        name: '@single-spa/welcome',
+        url: 'https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js',
+        activeWhen: ['/'],
+        exact: false,
+      },
+    ]);
+    applications.forEach(register);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    start({
+      urlRerouteOnly: true,
+    });
+  }
+})();
